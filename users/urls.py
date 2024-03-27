4dc7428fb36d9a5eb9957a85bdeb0a1871467a01
Carlos Vargas <esteban.vg00@gmail.com>
2024-03-27 09:26:06 -0400

    feat: API usuarios creada

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from users import views
+
+# API versioning
+router = routers.DefaultRouter()
+router.register(r'users', views.UserView, 'users')
+
+urlpatterns = [
+    path("api/v1/", include(router.urls))
+]
