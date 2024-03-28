d5f18b50c96e225e18a60f7591b35f92023f8cd6
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2024-03-27 20:01:10 -0400

    feat: SE realizo el formulario de registro y sus validaciones

--- frent/src/App.js
+++ frent/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { CustomerFormPage } from './components/Navbar/forms/customerFormPage';
 
 function App() {
   return (
     <>
-    
+    <CustomerFormPage></CustomerFormPage>
     </>
   );
 }
