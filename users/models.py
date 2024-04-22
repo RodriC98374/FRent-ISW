43c142d25637740e801786303280893b3c008188
link200309 <jonhh.0023@gmail.com>
2024-04-22 16:24:00 -0400

    para hacer pull 22/04

--- users/models.py
+++ users/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 from django.utils import timezone
 
-
-
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
