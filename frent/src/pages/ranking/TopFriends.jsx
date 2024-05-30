c034b18f14bde68de9d7c601ec8e68c938138426
RodriC98374 <rodricesp.3.0@gmail.com>
2024-05-29 20:09:57 -0400

    fix: Resueltos bugs AG - 48, AG - 49 y AG - 50

--- frent/src/pages/ranking/TopFriends.jsx
+++ frent/src/pages/ranking/TopFriends.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function TopFriends({friends}) {
   return (
@@ -7,9 +8,11 @@ export default function TopFriends({friends}) {
         {friends.map((friend, index) => (
             <div key={friend.id_user} className={`top-friend rank-${index + 1}`}>
               <div className="profile-picture">
-                  <img src={`data:image/png;base64,${friend.image}`} alt={`${friend.first_name}'s profile`} />
+                <Link to={`/profileFriend/${friend.id_user}`}>
+                    <img src={`data:image/png;base64,${friend.image}`} alt={`${friend.first_name}'s profile`} />
+                </Link>
               </div>
-              <div className="rank">{index + 1}</div>
+              <div className="rank">#{index + 1}</div>
               <div className="likes">{friend.like_count} <span role="img" aria-label="heart">❤️</span></div>
             </div>
         ))}
